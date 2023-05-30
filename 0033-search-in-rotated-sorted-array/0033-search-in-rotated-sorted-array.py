4b1eadf71db56640d926947f6390fd945f48e10d
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2023-05-30 19:33:08 +0300

    Time: 44 ms (75.89%), Space: 16.7 MB (13.81%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,44 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        peaknum = self.peak(nums)
+        
+        
+        if peaknum == None:
+            return(self.binary(nums, 0, len(nums)-1, target))
+        elif nums[peaknum] == target:
+            return peaknum
+        
+        return self.binary(nums, 0, peaknum,target) if target >= nums[0] else self.binary(nums,peaknum+1,len(nums)-1,target)
+        
+    
+    def binary(self, nums, start, end,target):
+        while start <= end:
+            mid = start + (end-start)//2
+            
+            if nums[mid] > target:
+                end = mid-1
+            elif nums[mid] < target:
+                start = mid+1
+            else:
+                return mid
+        return -1
+    
+    def peak(self, nums) -> int:
+        start = 0
+        end = len(nums) -1
+        
+        while start <= end:
+            mid = start + (end-start)//2
+            
+            if mid < end and nums[mid] > nums[mid+1]:
+                return mid
+            elif mid > start and nums[mid] < nums[mid-1]: 
+                return mid-1
+            
+            if nums[mid] >= nums[start]:
+                start =  mid+1
+            else:
+                end = mid-1
+         
+            
+        
