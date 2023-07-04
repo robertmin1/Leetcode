0a203aa5f97ec1a07e83fd0caf318d5a3cb968a2
robertmin1 <104002271+robertmin1@users.noreply.github.com>
2023-07-04 11:37:39 +0300

    Delete 0287-find-the-duplicate-number directory

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        nums = sorted(nums)
-        for num in range(len(nums)-1):
-            if nums[num] == nums[num+1]:
-                return nums[num]
