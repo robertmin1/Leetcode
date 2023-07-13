5927fe132e5645b6b88edae52a9cd73023869af7
Robert <104002271+robertmin1@users.noreply.github.com>
2023-07-13 12:40:20 +0300

    Time: 70 ms (33.82%), Space: 17.2 MB (38.80%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        characters=" ,:.@!`#$%^?\"&*()_-+={}[]\/';'"
-        for char in characters:
-            s = s.replace(char, "")
-        s =s.lower()
-        return s == ''.join(reversed(s))
+        palindrome = ""
+        for letter in s:
+            if letter.isalnum():
+                palindrome+=letter.lower()
+    
+        return palindrome == ''.join(reversed(palindrome))
