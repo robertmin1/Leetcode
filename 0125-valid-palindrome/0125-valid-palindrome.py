9ed31db1bd1f1220837fe5e27da01dc323ab737a
Robert <104002271+robertmin1@users.noreply.github.com>
2023-07-13 12:35:14 +0300

    Time: 59 ms (76.84%), Space: 17.2 MB (38.80%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,14 +4,4 @@ class Solution:
         for char in characters:
             s = s.replace(char, "")
         s =s.lower()
-        print(s)
-        return self.checker(s,0, len(s)-1)
-    
-    def checker(self, s:str, start:int, end:int) -> bool:
-        if start >= end:
-            return True
-        elif s[start] != s[end]:
-            return False
-        else:
-            return self.checker(s, start=start+1, end=end-1)
-        
+        return s == ''.join(reversed(s))
