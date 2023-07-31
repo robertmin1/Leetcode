d86bf131ac1f48d25ded608ca06dda2ec5bf1e5f
Robert <104002271+robertmin1@users.noreply.github.com>
2023-07-31 14:38:43 +0300

    Time: 128 ms (93.84%), Space: 17.3 MB (21.34%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -0,0 +1,25 @@
+class Solution:
+    def setZeroes(self, matrix: list[list[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        vars = []
+        def helper():
+            
+            for pos_row,m in enumerate(matrix):
+                   for pos_col,num in enumerate(m):
+                    print(pos_row,pos_col)
+                    if num == 0:
+                        print(pos_row,pos_col)
+                        vars.append([pos_row, pos_col])
+
+
+        helper()
+        
+        for var in vars:
+            a,b = var[0], var[1]
+            for num in range(len(matrix[0])):
+                matrix[a][num] = 0
+            for num in range(len(matrix)):
+                matrix[num][b] = 0
+        
